c442af82bc594bf6cb30eb8bf42fdc7e2037c74c
wai81 <50743966+wai81@users.noreply.github.com>
2021-09-19 10:51:38 +0300

    added mapping

--- Notes.Persistence/DbInitializer.cs
+++ Notes.Persistence/DbInitializer.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notes.Persistence
+﻿namespace Notes.Persistence
 {
     public class DbInitializer
     {
         public static void Initialize(NotesDbContext context)
         {
-            context.Database.EnsureCreated();
+            context.Database.EnsureCreated(); // создает базу данных если она несоздана при запуске
         }
     }
 }
